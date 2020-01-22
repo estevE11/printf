6ad9439e6012f287297c4a012ef46f3ab1318f07
Roger Esteve <Coconaut7@gmail.com>
2020-01-22 01:40:44 +0100

    Removed pip things

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="printf",
-    version="1.0.0",
-    description="Print formatted for Python",
-    py_modules=["printf"],
-    package_dir={"":"src"}
-)
