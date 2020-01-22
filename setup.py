1ea9c5b495e3af692b722f82d76610d53524297f
Roger Esteve <Coconaut7@gmail.com>
2020-01-22 01:38:04 +0100

    Automatic first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="printf",
+    version="1.0.0",
+    description="Print formatted for Python",
+    py_modules=["printf"],
+    package_dir={"":"src"}
+)
