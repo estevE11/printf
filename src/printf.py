1d5cd9b5df7d701ab4183f3872f56091faff89a3
Roger Esteve <Coconaut7@gmail.com>
2020-02-19 02:30:15 +0100

    Simplified the code

--- src/printf.py
+++ src/printf.py
@@ -3,15 +3,16 @@ def printf(s, *vars):
         print(s)
     else:
         res = ""
-        curr = ""
         i = 0
         for it in s:
             if it == "%":
-                res += curr
-                curr = ""
                 res += str(vars[i])
                 i += 1
             else:
-                curr += it
-        res += curr
+                res += it
         print(res)
+
+name = "Sailor"
+x = 4
+y = 5
+printf("% at (%, %)", name, x, y)
